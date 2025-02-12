0bbc2dbeaefb5f7a0566ccd5bae6888c8a51c6dc
Chris Burns <admin@chrisMacbook.local>
2025-02-11 19:14:00 -0500

    updat

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,7 +3,28 @@ import { auth } from './auth/resource';
 import { handler as contactUsFunction } from './function/contactUsFunction/src/index';
 import { handler as geoSpatialFunction } from './function/geoSpatialFunction/src/index';
 import { handler as chatFunction } from './function/chatFunction/src/index';
+import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
 
+// DynamoDB Tables
+const chatTable = new Table(this, 'ChatTable', {
+  partitionKey: { name: 'messageId', type: AttributeType.STRING },
+  tableName: 'Chat',
+  removalPolicy: 'DESTROY' // Optional: for dev environments
+});
+
+const contactTable = new Table(this, 'ContactTable', {
+  partitionKey: { name: 'contactId', type: AttributeType.STRING },
+  tableName: 'Contact',
+  removalPolicy: 'DESTROY'
+});
+
+const geoSpatialTable = new Table(this, 'GeoSpatialTable', {
+  partitionKey: { name: 'locationId', type: AttributeType.STRING },
+  tableName: 'GeoSpatial',
+  removalPolicy: 'DESTROY'
+});
+
+// Functions Factory
 const functionsFactory = {
   getInstance() {
     return {
@@ -19,4 +40,9 @@ const functionsFactory = {
 export default defineBackend({
   auth,
   functions: functionsFactory,
+  resources: {
+    chatTable,
+    contactTable,
+    geoSpatialTable,
+  },
 });
