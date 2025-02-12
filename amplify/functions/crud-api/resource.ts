ea163471bbe9ff9fcf6e445f9d5afaa3a87005a2
Chris Burns <admin@chrisMacbook.local>
2025-02-12 16:51:51 -0500

    updating functions

--- amplify/functions/crud-api/resource.ts
+++ amplify/functions/crud-api/resource.ts
@@ -1,5 +1,5 @@
-import { defineFunction } from "@aws-amplify/backend";
+import { defineFunction } from '@aws-amplify/backend';
 
 export const myApiFunction = defineFunction({
-  name: "api-function",
+  entry: '/index.ts',
 });
