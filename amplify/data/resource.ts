9287454dca5a6b59ecd0bf4e428b26e47932b7f5
Chris Burns <admin@chrisMacbook.local>
2025-02-13 21:04:59 -0500

    updating samples

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -38,6 +38,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "iam",
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
